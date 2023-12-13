3b6a0862e554d713ab2faa8e9cba4e5fe3fe1cc3
simimi129 <simimi129@gmail.com>
2023-12-13 23:41:34 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from 'core/navigation/navigation.component';
+import { HomeComponent } from 'modules/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavigationComponent } from 'core/navigation/navigation.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NavigationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
