fc916d51bd57fc372284a678b7427e54665493b0
simimi129 <simimi129@gmail.com>
2023-12-23 23:30:04 +0100

    adding animations

--- src/app/shared/components/todos-container/todos-container.component.ts
+++ src/app/shared/components/todos-container/todos-container.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
@@ -9,12 +16,25 @@ import { Todo } from 'shared/models/Todo.model';
   imports: [FontAwesomeModule],
   templateUrl: './todos-container.component.html',
   styleUrl: './todos-container.component.css',
+  animations: [
+    trigger('expandCollapse', [
+      state('closed', style({ height: '0', overflow: 'hidden' })),
+      state('open', style({ height: '*', overflow: 'hidden' })),
+      transition('closed <=> open', [animate('300ms ease-in-out')]),
+    ]),
+  ],
 })
 export class TodosContainerComponent {
   @Input({ required: true }) title!: string;
   @Input() todos: Todo[] = [];
 
   isOpen: boolean = false;
+  state: 'closed' | 'open' = 'closed';
+
+  togglePanel() {
+    this.isOpen = !this.isOpen;
+    this.state = this.isOpen ? 'open' : 'closed';
+  }
 
   faChevronUp = faChevronUp;
   faChevronDown = faChevronDown;
