b7345c7014223d5d1c4461ac599fdb197593f451
simimi129 <simimi129@gmail.com>
2023-12-24 11:01:36 +0100

    adding plus todo button

--- src/app/modules/todos/todos.component.ts
+++ src/app/modules/todos/todos.component.ts
@@ -2,16 +2,20 @@ import { Component } from '@angular/core';
 import { TodosContainerComponent } from 'shared/components/todos-container/todos-container.component';
 import { PanelDirective } from 'shared/directives/panel.directive';
 import { Todo } from 'shared/models/Todo.model';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [TodosContainerComponent],
+  imports: [TodosContainerComponent, FontAwesomeModule],
   hostDirectives: [PanelDirective],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css',
 })
 export class TodosComponent {
+  faPlus = faPlus;
+
   todosToday: Todo[] = [
     {
       id: 1,
